f9cd66ffffdfc2a6372dcddc7b78e59c9b54fba9
studbojers <sdebastian.bojers@stud.pointcollege.fi>
2016-05-19 11:02:17 +0300

    RC4: jQuery koodin siivous
    Scroll-to-top koodi pois html:n seasta js/oma.js tiedostoon.
    
    Historik-dokumentin nimen siivous.

--- js/oma.js
+++ js/oma.js
@@ -1,6 +1,13 @@
 
-
+/*
 function writeYears() {
     var yr = new Date().getFullYear();
     document.write('1945 - ' + yr.toString());
 }
+*/
+
+$(document).ready(function () {
+    $('.navigate-back-to-top').on('click', function () {
+        $('html,body').animate({scrollTop:0}, 'slow');
+    });
+});
