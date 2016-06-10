aa011e94ba0741e8a4b0f391fa6bf1552a802bed
studbojers <sdebastian.bojers@stud.pointcollege.fi>
2016-06-10 03:30:56 +0300

    Asiakkaan toiveisiin ja huomioihin perustuvia muutoksia
    - kaikkiin sähköposteihin [at] ja ohje, että korvattava @-merkillä
    - selitys, mitä personmedlem ja understödande tarkoittavat:
    Föredetta företagsmedlemmar kan ansöka om personmedlemsskap.
    - övriga styrelsemedlmmar rubrik ovanför övriga styrelsemedlemmar
    - epostadressen bort från bannern
    - document drive linken sist till menun => "Logga in"
    * uudelleenohjaus google driveen.
    - korjattu: verksamhetssida näkyy väärin iPadissa (musta teksti kuvan
    päällä ilman valkoista taustaa).
    - verksamhetssidalle lisäksi: Hösten 2016 HÖSTMÖTE Datumet bekräftas
    senare.
    -korjattu: MEDLEMSSKAPissa kirjoitusvirhe. Yksi ässä puuttuu!!!

--- js/oma.js
+++ js/oma.js
@@ -6,6 +6,69 @@ function writeYears() {
 }
 */
 
+function getBannerAndMenu() {
+    
+    var banner = "";
+    banner += '<div class="brand">Borgå Svenska KöpmannaFörening rf</div>';
+    //banner += '<!--<div class="address-bar"><script>writeYears();</script></div>-->';
+    //banner += '<div class="address-bar">borgakopmannaforening @ gmail.com</div>';
+
+    banner += '<!-- Navigation -->';
+    banner += '<nav class="navbar navbar-default" role="navigation">';
+    banner += '    <div class="container">';
+    banner += '        <!-- Brand and toggle get grouped for better mobile display -->';
+    banner += '        <div class="navbar-header">';
+    banner += '            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">';
+    banner += '                <span class="sr-only">Toggle navigation</span>';
+    banner += '                <span class="icon-bar"></span>';
+    banner += '                <span class="icon-bar"></span>';
+    banner += '                <span class="icon-bar"></span>';
+    banner += '            </button>';
+    banner += '            <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->';
+    banner += '            <a class="navbar-brand" href="index.html">Borgå Köpmannaförening</a>';
+    banner += '        </div>';
+    banner += '        <!-- Collect the nav links, forms, and other content for toggling -->';
+    banner += '        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">';
+    banner += '            <ul class="nav navbar-nav">';
+    banner += '                <li id="aframsida">';
+    banner += '                    <a href="index.html">Framsida</a>';
+    banner += '                </li>';
+    banner += '                <li id="ainfo" class="dropdown">';
+    banner += '                    <a class="dropdown-toggle" data-toggle="dropdown" href="info.html">Info<span class="caret"></span></a>';
+    banner += '                    <ul class="dropdown-menu">';
+    banner += '                        <li><a href="info.html#kontakt">Kontaktuppgifter</a></li>';
+    banner += '                        <li><a href="info.html#styrelse">Styrelse</a></li>';
+    banner += '                        <li><a href="info.html#bottom">Stadgar</a></li>';
+    banner += '                        <li><a href="info.html#bottom">Historik</a></li>';
+    banner += '                    </ul>';
+    banner += '                </li>';
+    banner += '                <li id="averksamhet">';
+    banner += '                    <a href="verksamhet.html">Verksamhet</a>';
+    banner += '                </li>';
+    banner += '                <li id="amedlemsskap" class="dropdown">';
+    banner += '                    <a  class="dropdown-toggle" data-toggle="dropdown" href="medlemskap.html">Medlemsskap<span class="caret"></span></a>';
+    banner += '                    <ul class="dropdown-menu">';
+    banner += '                        <li><a href="medlemskap.html#avgifter">Medlemsavgifter</a></li>';
+    banner += '                        <li><a href="medlemskap.html#foretagansokan">Ansökan om företagsmedlemsskap</a></li>';
+    banner += '                        <li><a href="medlemskap.html#bottom">Ansökan om personmedlemsskap</a></li>';
+    banner += '                        <li><a href="medlemskap.html#bottom">Ansökan till understödande medlem</a></li>';
+    banner += '                    </ul>';
+    banner += '                </li>';
+    banner += '                <li id="abilder">';
+    banner += '                    <a href="bilder.html">Bilder</a>';
+    banner += '                </li>';
+    banner += '                <li>';
+    banner += '                    <a href="skyddat/index.html">Logga in</a>';
+    banner += '                </li>';
+    banner += '            </ul>';
+    banner += '        </div>';
+    banner += '        <!-- /.navbar-collapse -->';
+    banner += '    </div>';
+    banner += '    <!-- /.container -->';
+    banner += '</nav>';
+    return banner;
+}
+
 function getBody1()
 {
     var body = 'Ansökan om företagsmedlemskap' + escape('\n');
@@ -43,14 +106,22 @@ function getBody2()
     return body;
 }
 
-/*
-function getBodyToo()
+function send(item)
 {
-    return document.getElementById('mailbody').innerHTML;
+    document.location.href = item;
 }
-*/
 
 $(document).ready(function () {
+    // get the name of the class of the '#banner-menu' div
+    // to set the 'active-navlink' selector to the correct menu item.
+    // activeNames are: aframsida, ainfo, averksamhet, amedlemsskap, abilder (one for each page)
+    var activeName = $('#banner-menu').attr('class');
+    $('#banner-menu').html(getBannerAndMenu());
+    $('*').removeClass('active-navlink');
+    $('#' + activeName).addClass('active-navlink');
+    
+    // -- event handlers -- //
+    
     $('.navigate-back-to-top').on('click', function () {
         $('html,body').animate({scrollTop:0}, 'slow');
     });
@@ -64,7 +135,7 @@ $(document).ready(function () {
         item += '&content-type=text/html';
         item += '&body=' + getBody1();
         
-        document.location.href = item;
+        send(item);
     });
     
     $('#nappi2').on('click', function() {
@@ -76,12 +147,14 @@ $(document).ready(function () {
         item += '&content-type=text/html';
         item += '&body=' + getBody2();
         
-        document.location.href = item;
+        send(item);
     });
     
 });
 
 
+/*
 $('#ansokan .dag').datepicker({
     format: "dd.mm.yyyy"
 });
+*/
